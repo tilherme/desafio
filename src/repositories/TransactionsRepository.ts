e9fa742aa5d5acc4144451f631d88d925a164650
tilherme <gui.mauricio@hotmail.com.br>
2020-11-17 16:05:17 -0300

    concluded

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,3 +1,7 @@
+/* eslint-disable prettier/prettier */
+// eslint-disable-next-line prettier/prettier
+/* eslint-disable spaced-comment */
+/* eslint-disable prettier/prettier */
 import { EntityRepository, Repository } from 'typeorm';
 
 import Transaction from '../models/Transaction';
@@ -10,8 +14,35 @@ interface Balance {
 
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
-  public async getBalance(): Promise<Balance> {
-    // TODO
+public async getBalance(): Promise<Balance> {
+    const transactions = await this.find();
+
+    const {income, outcome} = transactions.reduce((accumulator, transaction) =>{
+      switch (transaction.type) {
+        case "income":
+          // eslint-disable-next-line no-param-reassign
+          accumulator.income += Number(transaction.value);
+          break;
+          
+          case "outcome":
+            // eslint-disable-next-line no-param-reassign
+            accumulator.outcome += Number(transaction.value);
+            break;
+
+            default:
+              break;         
+
+      }
+      return accumulator;
+    },
+    {
+      income:0,
+      outcome:0,
+      total:0,
+    },
+    ); 
+      const total = income - outcome;
+      return {income , outcome, total};
   }
 }
 
--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,24 @@
-// import AppError from '../errors/AppError';
+/* eslint-disable prettier/prettier */
+import {getCustomRepository} from 'typeorm';
 
+import AppError from '../errors/AppError';
+
+ // eslint-disable-next-line @typescript-eslint/no-unused-vars
+ import Transaction from '../models/Transaction';
+
+  // eslint-disable-next-line import/newline-after-import
+  import TransactionsRepository from '../repositories/TransactionsRepository'
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<void> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    const transaction = await transactionsRepository.findOne(id);
+
+    if(!transaction) {
+      throw new AppError('Transaction does not exist')
+    }
+
+   await transactionsRepository.remove(transaction);
   }
 }
 
